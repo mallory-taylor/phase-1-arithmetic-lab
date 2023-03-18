73f3238cbab73867f4038724fb6f2e05c03462d3
Mallory Taylor <malloryataylor13@gmail.com>
2023-03-17 23:09:43 -0400

    Arithmetic expressions

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+
+let max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
+
+let random = Math.floor(Math.random() * 10) + 1;
